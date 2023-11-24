243d7618328a58e0d4b9f8ee5b80adf93307d11e
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-11-24 19:37:25 +0800

    home/hgh: enable global git ignore, with vscode config

--- home/hgh/git.nix
+++ home/hgh/git.nix
@@ -3,6 +3,12 @@
     enable = true;
     userName = "Guanghui Hu";
     userEmail = "120L052208@stu.hit.edu.cn";
+    ignores = [
+      "*~"
+      "*.swp"
+      ".direnv/"
+      ".envrc"
+    ];
     extraConfig = {
       init = {
         defaultBranch = "main";
