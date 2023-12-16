a68752d4de88ab9bb87b5db1eef9836c084c4e71
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-12-16 21:16:20 +0800

    module/home: vscode add todo tree plugin

--- modules/home/vscode/default.nix
+++ modules/home/vscode/default.nix
@@ -10,6 +10,8 @@ with lib; let cfg = config.euphgh.home.vscode; in
         vscodevim.vim
         # color theme
         arcticicestudio.nord-visual-studio-code
+        # todo highlight
+        gruntfuggly.todo-tree
         # icon theme
         pkief.material-icon-theme
         # git
