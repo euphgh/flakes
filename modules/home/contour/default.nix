7390b8d40265ce396e664da95dd0abd8f814e573
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-12-27 17:17:08 +0800

    modules/home: fix contour module grammar

--- modules/home/contour/default.nix
+++ modules/home/contour/default.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 with lib; let cfg = config.euphgh.home.contour; in
 {
-  options.euphgh.home.contour.enable = mkEnableOption "euphgh alacritty";
+  options.euphgh.home.contour.enable = mkEnableOption "euphgh contour";
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       contour
--- modules/home/default.nix
+++ modules/home/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./alacritty
     ./kitty
+    ./contour
     ./nvim
     ./tmux
     ./vscode
