0f28e298d538f216e87388fefaf24b1c75a99719
Guanghui Hu <120L052208@stu.hit.edu.cn>
2024-01-21 14:27:10 +0800

    feat(packages): add verilator-4.218

--- packages/default.nix
+++ packages/default.nix
@@ -1,4 +1,5 @@
 { nixpkgs ? import <nixpkgs> { }, ... }@inputs: {
   millw = nixpkgs.callPackage ./millw.nix { };
   verilator_5016 = nixpkgs.callPackage ./verilator_5016.nix { };
+  verilator_4218 = nixpkgs.callPackage ./verilator_4218.nix { };
 }
