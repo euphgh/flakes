a9a4b5d4029e524aa07c2bd0816f4d8e37f0d383
Guanghui Hu <120L052208@stu.hit.edu.cn>
2024-02-01 22:19:47 +0800

    fix(home): hgh use old-vlc

--- home/hgh/default.nix
+++ home/hgh/default.nix
@@ -1,8 +1,10 @@
 { pkgs, self, system, lib, ... }:
 let
-  millw-alias-mill = self.packages.${system}.millw.override {
-    alias = "mill";
+  millw-alias-mill = self.packages.${system}.millw.override { alias = "mill"; };
+  vlc-old = pkgs.libsForQt5.qt5.callPackage ./vlc-old.nix {
+    # inherit(pkgs.qt6) qtbase;
   };
+  # vlc-old = self.packages.${system}.vlc-old;
 in
 {
   imports = [ ./git.nix ];
@@ -47,7 +49,7 @@ in
 
     # vedio
     qbittorrent
-    vlc
+    vlc-old
   ];
 
   home = {
