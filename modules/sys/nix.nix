82f10a5a7745f0a972dc7895edee5a2cd69ea624
Guanghui Hu <hugh2024@shanghaitech.edu.cn>
2025-02-04 17:19:06 +0800

    dev: add ustc mirror

--- modules/sys/nix.nix
+++ modules/sys/nix.nix
@@ -6,6 +6,7 @@
   nix = {
     settings = {
       substituters = [
+        "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://mirrors.bfsu.edu.cn/nix-channels/store"
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
         "https://nix-community.cachix.org"
