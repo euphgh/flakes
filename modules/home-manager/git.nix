6f4e282e2a5f789b280b19026dcc581b351da12d
Guanghui Hu <hugh2024@shanghaitech.edu.cn>
2026-02-26 11:26:33 +0800

    refact git configure

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,8 +1,6 @@
 { ... }: {
   programs.git = {
     enable = true;
-    userName = "Guanghui Hu";
-    userEmail = "hugh2024@shanghaitech.edu.cn";
     ignores = [
       "*~"
       "*.swp"
@@ -10,26 +8,30 @@
       ".cache/"
       ".vscode/"
     ];
-    extraConfig = {
+    settings = {
+      user = {
+        name = "Guanghui Hu";
+        email = "hugh2024@shanghaitech.edu.cn";
+      };
       init = {
         defaultBranch = "main";
       };
       core = {
         editor = "nvim";
       };
-    };
-    aliases = {
-      st = "status -sb";
-      cm = "commit -m";
-      rv = "remote -v";
-      last = "log -1 HEAD --stat";
-      d = "diff";
-      gl = "config --global -l";
-      se = "!git rev-list --all | xargs git grep -F";
-      rt = "!cd $(git rev-parse --show-toplevel)"; #root
-      co = "checkout";
-      ll = "log --oneline";
-      ig = "!echo \"$@\" >> .git/info/exclude";
+      alias = {
+        st = "status -sb";
+        cm = "commit -m";
+        rv = "remote -v";
+        last = "log -1 HEAD --stat";
+        d = "diff";
+        gl = "config --global -l";
+        se = "!git rev-list --all | xargs git grep -F";
+        rt = "!cd $(git rev-parse --show-toplevel)"; #root
+        co = "checkout";
+        ll = "log --oneline";
+        ig = "!echo \"$@\" >> .git/info/exclude";
+      };
     };
   };
 }
